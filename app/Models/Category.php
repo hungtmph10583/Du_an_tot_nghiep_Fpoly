721902fba590105d3dd28a63e1922833c34f313a
hoanganhhn596 <67960974+HoangAnh596@users.noreply.github.com>
2021-09-08 22:40:46 +0700

    abc

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     use HasFactory;
+    protected $table = 'categories';
+    public $fillable = [
+        'name','status','show_menu',
+    ];
+    public function books(){
+        return $this->hasMany(Book::class, 'cate_id');
+    }
 }
