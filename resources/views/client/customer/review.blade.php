bd74748ea107f6d936ed71cc4dabbda2175fb9e6
tr.ma.h___] <84056565+hungtmph10583@users.noreply.github.com>
2021-11-15 20:21:05 +0700

    Update review.blade.php

--- resources/views/client/customer/review.blade.php
+++ resources/views/client/customer/review.blade.php
@@ -17,7 +17,7 @@
             </div>
             <ul>
                 <li>
-                    <a href="{{route('client.customer.info')}}" class="active">
+                    <a href="{{route('client.customer.info')}}">
                         <i class="fas fa-user"></i>
                         Thông tin tài khoản
                     </a>
@@ -35,7 +35,7 @@
                     </a>
                 </li>
                 <li>
-                    <a href="{{route('client.customer.review')}}">
+                    <a href="{{route('client.customer.review')}}" class="active">
                         <i class="fas fa-star-half-alt"></i>
                         Nhận xét của tôi
                     </a>
@@ -44,9 +44,53 @@
         </div>
         <div class="account-info-right">
             <div class="title">Đánh giá sản phẩm</div>
-            <div class="group">
-                <img src="{{ asset('client-theme/images/error.jpg')}}" alt="error">
-            </div>
+            <table class="greenTable">
+                <thead>
+                    <tr>
+                        <th>Sản phẩm</th>
+                        <th>Xếp hạng sp</th>
+                        <th>Nội dung đánh giá</th>
+                        <th>Thời gian</th>
+                        <td>Hành động</td>
+                    </tr>
+                </thead>
+                <tbody class="list-overflow">
+                    @foreach($review as $rv)
+                        @foreach($product as $pro)
+                            @if($rv->product_id == $pro->id)
+                            <tr>
+                                <td>
+                                    <a href="{{route('client.product.detail', ['id' => $rv->product->id])}}">
+                                        <img src="{{asset( 'storage/' . $rv->product->image)}}" alt="Sản phẩm này hiện chưa có ảnh hoặc ảnh bị lỗi hiển thị!" width="100">
+                                    </a>
+                                </td>
+                                <td>
+                                    <span class="star">
+                                       @for($count=1; $count<=5; $count++)
+                                            @if($count <= $pro->reviews->rating)
+                                                <i class="fas fa-star rating"></i>
+                                            @else
+                                                <i class="far fa-star"></i>
+                                            @endif
+                                        @endfor
+                                    </span>
+                                </td>
+                                <td class="comment">{{$rv->comment}}</td>
+                                <td>{{date_format($rv->created_at,"d/m/Y H:i:s")}}</td>
+                                <td>
+                                    <a href="#" class="edit-review">
+                                        <i class="far fa-edit"></i>
+                                    </a>
+                                    <a href="{{route('deleteReview', ['id' => $rv->id])}}" onclick="return confirm('Bạn có chắc muốn xóa review này?')" class="delete-review">
+                                        <i class="far fa-trash-alt"></i>
+                                    </a>
+                                </td>
+                            </tr>
+                            @endif
+                        @endforeach
+                    @endforeach
+                </tbody>
+            </table>
         </div>
     </div>
 </section>
