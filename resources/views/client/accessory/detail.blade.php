26002b5cd9fbf0596b9036cdeec20efe25ac120a
tr.ma.h___] <84056565+hungtmph10583@users.noreply.github.com>
2021-12-07 04:17:58 +0700

    Update detail.blade.php

--- resources/views/client/accessory/detail.blade.php
+++ resources/views/client/accessory/detail.blade.php
@@ -125,113 +125,128 @@
         </div>
     </div>
 </section>
-<section class="product-tab">
-    <div class="product-tabs-container">
-        <div class="product-list-tabs">
-            <button id="btn-product-description" class="one">Mô tả sản phẩm</button>
-            <button id="btn-reviews" class="three">Đánh giá</button>
-        </div>
-        <div class="product-tabs-content product-description active">
-            <h5>Product desription</h5>
-            <p>Đặc điểm nhận dạng: Lông vũ sặc sỡ, hai mắt, hai chân, hai cánh</p>
-            <p>Cân nặng 5kg</p>
-            <p>Giống đực</p>
-            <p>Chủng loại: vẹt Bắc Âu</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat enim rerum reiciendis aperiam? Veniam explicabo distinctio quod, pariatur quasi dolorum assumenda accusantium iusto voluptatum voluptate modi dolore. Id, tempora consequatur.</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat enim rerum reiciendis aperiam? Veniam explicabo distinctio quod, pariatur quasi dolorum assumenda accusantium iusto voluptatum voluptate modi dolore. Id, tempora consequatur.</p>
-        </div>
-        <div class="product-tabs-content reviews">
-            <h5>Phản hồi từ khách hàng</h5>
-            <div class="review-content comment">
-                <span class="star">
-                    @for($count=1; $count<=5; $count++)
-                        @if($count <= $rating)
-                            <i class="fas fa-star rating"></i>
-                        @elseif($countReview == 0)
-                            <i class="fas fa-star rating"></i>
-                        @else
-                            <i class="far fa-star"></i>
-                        @endif
-                    @endfor
-                </span>
-                <span class="total-start">Đựa trên {{$countReview}} nhận xét</span>
-                <button id="btn-write-comment">Viết nhận xét</button>
-                <div class="clear-both"></div>
-            </div>
-            <div class="write-comment">
-                <p>Viết đánh giá</p>
-                <form action="{{route('client.accessory.post_review')}}" method="POST" enctype="multipart/form-data">
-                @csrf
-                    <input type="hidden" name="product_type" value="2">
-                    <input type="hidden" name="product_id" value="{{$model->id}}">
-                    <div class="form-group">
-                        <label for="">Name</label>
-                        @if(Auth::check())
-                        <input type="text" name="name" placeholder="Nhập vào họ tên" value="{{Auth::user()->name}}">
-                        @else
-                        <input type="text" name="name" placeholder="Nhập vào họ tên">
-                        @endif
-                    </div>
-                    <div class="form-group">
-                        <label for="">Email</label>
-                        @if(Auth::check())
-                        <input type="text" name="email" placeholder="Nhập vào email" value="{{Auth::user()->email}}">
-                        @else
-                        <input type="text" name="email" placeholder="Nhập vào email">
-                        @endif
-                    </div>
-                    <div class="form-group">
-                        <label for="">Đánh giá sao</label>
-                        <span class="star-widget">
-                            <input type="radio" value="5" name="rating" id="rate-5">
-                            <label for="rate-5" class="fas fa-star"></label>
-                            <input type="radio" value="4" name="rating" id="rate-4">
-                            <label for="rate-4" class="fas fa-star"></label>
-                            <input type="radio" value="3" name="rating" id="rate-3">
-                            <label for="rate-3" class="fas fa-star"></label>
-                            <input type="radio" value="2" name="rating" id="rate-2">
-                            <label for="rate-2" class="fas fa-star"></label>
-                            <input type="radio" value="1" name="rating" id="rate-1">
-                            <label for="rate-1" class="fas fa-star"></label>
-                        </span>
-                        <div class="clear-both"></div>
-                    </div>
-                    <div class="form-group">
-                        <label for="">Nội dung đánh giá</label>
-                        <textarea name="comment" id="" cols="30" rows="10" placeholder="Viết nội dung dánh giá của bạn tại đây"></textarea>
-                    </div>
-                    <button type="submit" class="btn">Submit</button>
-                </form>
-            </div>
-            @foreach($review as $rv)
-                @if($rv->product_id == $model->id)
-                <div class="review-content">
-                    <div class="star">
+<section class="product_description">
+    <h2>Mô tả sản phẩm</h2>
+    <div class="description">
+        {{$model->description}}
+    </div>
+</section>
+<section class="review_custom">
+    <div class="customer_reviews">
+        <h2>Đánh giá từ khách hàng</h2>
+        <div class="container_customer_review">
+            <div class="see_review">
+                <div class="totail_rating">
+                    <span class="rating">
                         @for($count=1; $count<=5; $count++)
-                            @if($count <= $rv->rating)
+                            @if($count <= $rating)
+                                <i class="fas fa-star rating"></i>
+                            @elseif($countReview == 0)
                                 <i class="fas fa-star rating"></i>
                             @else
                                 <i class="far fa-star"></i>
                             @endif
                         @endfor
-                    </div>
-                    <p class="name-custom-review">{{$rv->user->name}}</p>
-                    <span class="date-custom-review">{{$rv->created_at->diffForHumans()}}</span>
-                    <div class="content">
-                        <p>{{$rv->comment}}</p>
-                    </div>
+                    </span>
+                    <span>Rating: {{$rating}} ({{$countReview}} đánh giá)</span>
                 </div>
-                @endif
-            @endforeach
-            @if($countReview < 1)
-            <div class="review-content">
-                <div class="content">
-                    <p>Chưa có bình luận nào</p>
+                <div class="content_review">
+                    @foreach($review as $rv)
+                        @if($rv->product_id == $model->id)
+                        <div class="box-review">
+                            <div class="calendar">
+                                <span>
+                                    {{date('d/m', strtotime($rv->created_at))}}
+                                </span>
+                                <span>{{date('Y', strtotime($rv->created_at))}}</span>
+                            </div>
+                            <div class="content">
+                                <span class="rating">
+                                    @for($count=1; $count<=5; $count++)
+                                        @if($count <= $rv->rating)
+                                            <i class="fas fa-star rating"></i>
+                                        @else
+                                            <i class="far fa-star"></i>
+                                        @endif
+                                    @endfor
+                                </span>
+                                <div class="review_body">
+                                    {{$rv->comment}}
+                                </div>
+                                <div class="review_author">
+                                    {{$rv->user->name}}
+                                </div>
+                            </div>
+                        </div>
+                        @endif
+                    @endforeach
+                    @if(empty(count($review)))
+                    <div class="box-review">
+                            <div class="content">
+                                <div class="review_author">
+                                    Sản phẩm này chưa có Review từ khách hàng!
+                                </div>
+                            </div>
+                        </div>
+                    @endif
                 </div>
             </div>
-            @endif
-            <div class="paging">
-            {{ $review->links('vendor.pagination.custom') }}
+            <div class="form_review">
+                <h3>Gửi đánh giá của bạn</h3>
+                <span class="note_comment">(Bạn có thể dùng email đã mua hàng để comment nếu không có tài khoản!)</span>
+                <form action="{{route('client.accessory.post_review')}}" method="POST" enctype="multipart/form-data">
+                @csrf
+                    <input type="hidden" name="product_type" value="2">
+                    <input type="hidden" name="product_id" value="{{$model->id}}">
+                    <div class="box">
+                        <div class="col">
+                            @if(Auth::check())
+                            <input type="text" name="name" placeholder="Name" value="{{Auth::user()->name}}">
+                            @else
+                            <input type="text" name="name"  placeholder="Name"value="{{old('name')}}">
+                            @endif
+                        </div>
+                        @error('name') <div class="text-danger">{{ $message }}</div> @enderror
+                    </div>
+                    <div class="box">
+                        <div class="col">
+                            @if(Auth::check())
+                            <input type="text" name="email" placeholder="Email" value="{{Auth::user()->email}}">
+                            @else
+                            <input type="text" name="email" placeholder="Email" value="{{old('email')}}">
+                            @endif
+                        </div>
+                        @error('email') <div class="text-danger">{{ $message }}</div> @enderror
+                    </div>
+                    <div class="box">
+                        <div class="col col-star">
+                            <label for="">Đánh giá:</label>
+                            <span class="star-widget">
+                                <input type="radio" value="5" name="rating" id="rate-5" @if( old('rating') == 5 ) checked  @endif>
+                                <label for="rate-5" class="fas fa-star"></label>
+                                <input type="radio" value="4" name="rating" id="rate-4" @if( old('rating') == 4 ) checked  @endif>
+                                <label for="rate-4" class="fas fa-star"></label>
+                                <input type="radio" value="3" name="rating" id="rate-3" @if( old('rating') == 3 ) checked  @endif>
+                                <label for="rate-3" class="fas fa-star"></label>
+                                <input type="radio" value="2" name="rating" id="rate-2" @if( old('rating') == 2 ) checked  @endif>
+                                <label for="rate-2" class="fas fa-star"></label>
+                                <input type="radio" value="1" name="rating" id="rate-1" @if( old('rating') == 1 ) checked  @endif>
+                                <label for="rate-1" class="fas fa-star"></label>
+                            </span>
+                            <div class="clear-both"></div>
+                        </div>
+                        @error('rating') <div class="text-danger">{{ $message }}</div> @enderror
+                    </div>
+                    <div class="box">
+                    <div class="col">
+                            <textarea name="comment" placeholder="Review" id="" cols="30" rows="10">{{ old('comment') }}</textarea>
+                        </div>
+                        @error('comment') <div class="text-danger">{{ $message }}</div> @enderror
+                    </div>
+                    <div class="box-last">
+                        <button>Gửi đánh giá của bạn</button>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
@@ -295,7 +310,7 @@
 
     for (var i = 0; i < thumbnails.length; i++) {
 
-        thumbnails[i].addEventListener('mouseover', function() {
+        thumbnails[i].addEventListener('click', function() {
             // console.log(activeImages)
             if (activeImages.length > 0) {
                 activeImages[0].classList.remove('active')
@@ -316,29 +331,6 @@
     buttonRight.addEventListener('click', function() {
         document.getElementById('slide-thumbnails').scrollLeft += 180
     })
-
-    let productDescription = document.querySelector('.product-description');
-    let reviews = document.querySelector('.reviews');
-    let writeCommment = document.querySelector('.write-comment');
-    var btnPD = document.getElementById('btn-product-description');
-    var btnRV = document.getElementById('btn-reviews');
-    var btnRVC = document.getElementById('btn-write-comment');
-    document.addEventListener('click', function(event) {
-        var isClickInsidePD = btnPD.contains(event.target);
-        var isClickInsideRV = btnRV.contains(event.target);
-        var isClickInsideRVC = btnRVC.contains(event.target);
-        if (isClickInsidePD) {
-            productDescription.classList.add('active');
-            reviews.classList.remove('active');
-        }
-        if (isClickInsideRV) {
-            reviews.classList.add('active');
-            productDescription.classList.remove('active');
-        }
-        if (isClickInsideRVC) {
-            writeCommment.classList.toggle('active');
-        }
-    });
 
     var swiper = new Swiper(".product-slider", {
             spaceBetween: 10,
