d863ad31aade92715b80004e5aaaa3c1bf519e1e
tr.ma.h___] <84056565+hungtmph10583@users.noreply.github.com>
2021-12-07 04:15:01 +0700

    Update index.blade.php

--- resources/views/client/product/index.blade.php
+++ resources/views/client/product/index.blade.php
@@ -18,7 +18,7 @@
                         <option value="">Tìm kiếm theo danh mục</option>
                         @foreach($category as $cate)
                             @if($cate->category_type_id == 1)
-                            <option value="{{$cate->id}}">{{$cate->name}}t</option>
+                            <option value="{{$cate->id}}">{{$cate->name}}</option>
                             @endif
                         @endforeach
                     </select>
@@ -26,6 +26,7 @@
                 <div class="form-item">
                     <!-- <label for="">Sắp xếp theo</label> -->
                     <select name="" id="">
+                        <option value="">Thứ tự mặc định</option>
                         <option value="">Giá cao nhất</option>
                         <option value="">Giá thấp nhất</option>
                         <option value="">Mua nhiều nhất</option>
@@ -50,18 +51,20 @@
                     </a>
                 </div>
                 <div class="product-extra">
-                    <form action="{{route('buyNow')}}" method="POST" enctype="multipart/form-data">
-                    @csrf
-                        <input type="hidden" name="product_id_hidden" value="{{$p->id}}">
-                        <input type="hidden" name="discount_price" value="{{$p->discount}}">
-                        <input type="hidden" name="quantity" value="1">
-                        <button type="submit" class="btn-buyNow">Mua hàng</button>
-                    </form>
+                    <form action="{{route('saveCart')}}" method="POST" enctype="multipart/form-data">
+                        @csrf
+                            <input type="hidden" name="product_id_hidden" value="{{$p->id}}">
+                            <input type="hidden" name="product_type" value="1">
+                            <input type="hidden" name="discount_price" value="{{$p->discount}}">
+                            <input type="hidden" name="category_id" value="{{$p->category_id}}">
+                            <input type="hidden" name="quantity" value="1">
+                            <button type="submit" class="btn-buyNow">Thêm vào giỏ hàng</button>
+                        </form>
                 </div>
             </div>
             <div class="item-bottom">
                 <div class="product-info">
-                    <a href="{{route('client.product.detail', ['id' => $p->id])}}" class="name">{{$p->name}}</a>
+                    <a href="{{route('client.product.detail', ['id' => $p->slug])}}" class="name">{{$p->name}}</a>
                     @if($p->discount == '')
                         <span class="price">{{number_format($p->price)}}đ</span>
                     @else
