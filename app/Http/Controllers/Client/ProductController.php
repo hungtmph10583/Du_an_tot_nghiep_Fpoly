06fc86cae03cf5fd3340097233dbd6e7f06c86fe
tr.ma.h___] <84056565+hungtmph10583@users.noreply.github.com>
2021-12-07 04:24:03 +0700

    Update ProductController.php

--- app/Http/Controllers/Client/ProductController.php
+++ app/Http/Controllers/Client/ProductController.php
@@ -12,6 +12,8 @@ use App\Models\Gender;
 use App\Models\Age;
 use App\Models\ProductGallery;
 use App\Models\Review;
+use App\Models\Order;
+use App\Models\OrderDetail;
 use App\Models\GeneralSetting;
 use Illuminate\Support\Facades\Auth;
 
@@ -22,10 +24,10 @@ class ProductController extends Controller
         $searchData = $request->except('page');
         if(count($request->all()) == 0){
             //Lấy ra danh sách sản phẩm & phân trang cho nó
-            $products = Product::paginate($pagesize);
+            $products = Product::orderBy('created_at', 'DESC')->paginate($pagesize);
         }else{
             $productQuery = Product::where('name', 'like', "%" .$request->keyword . "%");
-            $products = $productQuery->paginate($pagesize)->appends($searchData);
+            $products = $productQuery->orderBy('created_at', 'DESC')->paginate($pagesize)->appends($searchData);
         }
         $products->load('category', 'breed', 'gender');
         
@@ -62,22 +64,69 @@ class ProductController extends Controller
         $countReview = Review::where('product_id',$model->id)->where('product_type', '1')->count();
         $rating = Review::where('product_id', $model->id)->where('product_type', '1')->avg('rating');
         $rating = (int)round($rating);
+        
         return view('client.product.detail', compact('category', 'model', 'breed', 'gender', 'review', 'rating', 'countReview', 'generalSetting', 'product_slide'));
     }
 
     public function saveReview(Request $request){
-        $user = Auth::user();
-        if($user == null) {
-            return redirect()->back()->with('danger', 'Vui lòng đăng nhập để nhận xét')->withInput();
+        $request->validate(
+            [
+                'name' => 'required',
+                'email' => 'required',
+                'rating' => 'required',
+                'comment' => 'required'
+            ],
+            [
+                'name.required' => "Hãy nhập vào tên của bạn!",
+                'email.required' => "Hãy nhập vào email!",
+                'rating.required' => "Hãy chọn đánh giá sao!",
+                'comment.required' => "Hãy Nhập vào nội dung!"
+            ]
+        );
+
+        $check_review = Review::where('email', $request->email)->where('product_id', $request->product_id)->where('product_type', $request->product_type)->first();
+        $check_order = Order::where('email', $request->email)->where('delivery_status', '3')->get();
+        if (!empty(count($check_order))) {
+            foreach ($check_order as $key => $value) {
+                $check_order_detail = OrderDetail::where('order_id', $value->id)->where('product_id', $request->product_id)->where('product_type', $request->product_type)->get();
+            }
+            if (!empty(count($check_order_detail))) {
+               if (!empty($check_review)) {
+                    $find_review = Review::find($check_review->id);
+                    $find_review->fill($request->all());
+                    $find_review->product_id =$request->product_id;
+                    $find_review->product_type =$request->product_type;
+                    if (Auth::check()) {
+                        $find_review->user_id = Auth::user()->id;
+                    }
+                    $find_review->name = $request->name;
+                    $find_review->email = $request->email;
+                    $find_review->rating = $request->rating;
+                    $find_review->comment = $request->comment;
+                    $find_review->status = true;
+                    $find_review->save();
+                    return redirect()->back()->with('success', 'Cập nhật nhận xét thành công')->withInput();
+                }else{
+                    $review = new Review;
+                    $review->fill($request->all());
+                    $review->product_id =$request->product_id;
+                    $review->product_type =$request->product_type;
+                    if (Auth::check()) {
+                        $review->user_id = Auth::user()->id;
+                    }
+                    $review->name = $request->name;
+                    $review->email = $request->email;
+                    $review->rating = $request->rating;
+                    $review->comment = $request->comment;
+                    $review->status = true;
+                    $review->save();
+                    return redirect()->back()->with('success', 'Nhận xét thành công')->withInput();
+                }
+            }else{
+                return redirect()->back()->with('danger', 'Đơn hàng của bạn đang được xử lí. Quay lại bình luận khi bạn đã nhận được hàng!')->withInput();
+            }
+        }else{
+            return redirect()->back()->with('danger', 'Email này chưa từng dùng để mua sản phẩm này. Vui lòng thử lại')->withInput();
         }
-        $review = new Review;
-        $review->product_id =$request->product_id;
-        $review->product_type =$request->product_type;
-        $review->user_id = $user->id;
-        $review->rating = $request->rating;
-        $review->comment = $request->comment;
-        $review->status = true;
-        $review->save();
-        return redirect()->back()->with('success', 'Nhận xét thành công')->withInput();
     }
 }
